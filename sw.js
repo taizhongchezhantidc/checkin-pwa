675bd6e2dc5a7f36976c87b896ea54ac32c31be1
taizhongchezhantidc <taizhongchezhantidc@gmail.com>
2025-11-20 23:22:46 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME="checkin-pwa-v1";
-const URLS=["./","./index.html","./manifest.json"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS))); self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
